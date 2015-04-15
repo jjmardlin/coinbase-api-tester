11cedc8de24eae290c2e1c762156fbec2f904cdd
jborseth <jborseth@users.noreply.github.com>
2015-04-15 15:14:53 -0400

    Add 02_show_an_account.js

---
+++ methods/accounts/02_show_an_account.js
@@ -0,0 +1,37 @@
+/**
+ * Show an account
+ *
+ * Docs:
+ *   https://developers.coinbase.com/api#show-an-account
+ * Lib:
+ *   Client.prototype.getAccount
+ *   https://github.com/coinbase/coinbase-node/blob/master/lib/Client.js
+ */
+
+var client = require('../../client.js');
+var async  = require('async');
+
+async.waterfall([
+  function(callback) {
+    // Fetch accounts to get an account ID
+    client.getAccounts(function(err, accounts) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        callback(null, accounts[0].id);
+      }
+    });
+  }, function(accountId, callback) {
+
+    // Optionally, you can manually specify an account ID here if needed
+    // var accountId = '';
+
+    client.getAccount(accountId, function(err, account) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log(account);
+      }
+    });
+  }
+]);
