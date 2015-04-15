cd254d93a4736f58bb66678172828ce706160d78
jborseth <jborseth@users.noreply.github.com>
2015-04-15 15:12:52 -0400

    Add 01_list_accounts.js

---
+++ methods/accounts/01_list_accounts.js
@@ -0,0 +1,19 @@
+/**
+ * List accounts
+ *
+ * Docs:
+ *   https://developers.coinbase.com/api#list-accounts
+ * Lib:
+ *   Client.prototype.getAccounts
+ *   https://github.com/coinbase/coinbase-node/blob/master/lib/Client.js
+ */
+
+var client = require('../../client.js');
+
+client.getAccounts(function(err, accounts) {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(accounts);
+  }
+});
