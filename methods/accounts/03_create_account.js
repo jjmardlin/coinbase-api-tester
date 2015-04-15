505beabdc83ee8699084b2ff47f9b74c65705f9a
jborseth <jborseth@users.noreply.github.com>
2015-04-15 15:57:06 -0400

    Add 03_create_account.js

---
+++ methods/accounts/03_create_account.js
@@ -0,0 +1,25 @@
+/**
+ * Create an account
+ *
+ * Docs:
+ *   https://developers.coinbase.com/api#create-account
+ * Lib:
+ *   Client.prototype.createAccount
+ *   https://github.com/coinbase/coinbase-node/blob/master/lib/Client.js
+ */
+
+var client = require('../../client.js');
+
+var sampleData = {
+  "account": {
+    "name": "Savings Wallet"
+  }
+};
+
+client.createAccount(sampleData, function(err, response) {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(response);
+  }
+});
