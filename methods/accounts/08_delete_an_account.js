1a93ba8a011ea5ab511466ea615861948b679e5c
jborseth <jborseth@users.noreply.github.com>
2015-05-06 12:28:14 -0400

    Add 08_delete_an_account.js

---
+++ methods/accounts/08_delete_an_account.js
@@ -0,0 +1,44 @@
+/**
+ * Delete an account
+ *
+ * Only non-primary accounts with zero balance can be deleted.
+ *
+ * Docs:
+ *   https://developers.coinbase.com/api#delete-an-account
+ * Lib:
+ *   Account.prototype.delete
+ *   https://github.com/coinbase/coinbase-node/blob/master/lib/model/Account.js
+ */
+
+var async   = require('async');
+var Account = require('coinbase').model.Account;
+var client  = require('../../client.js');
+
+
+async.waterfall([
+  function(callback) {
+    // Fetch an account
+    client.getAccounts(function(err, accounts) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        // set this to a non-primary account
+        callback(null, accounts[2]);
+      }
+    });
+  }, function(sampleAccount, callback) {
+
+    var myAccount = new Account(client, sampleAccount);
+
+    // To manually specify an account ID
+    // var myAccount = new Account(client, {'id': 'A1234'});
+
+    myAccount.delete(function(err, result) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log(result);
+      }
+    });
+  }
+]);
