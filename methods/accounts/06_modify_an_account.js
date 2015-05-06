1ae39efd069461fa7411fb59ed82b7941f56fec9
jborseth <jborseth@users.noreply.github.com>
2015-05-06 12:15:24 -0400

    Add 06_modify_an_account.js

---
+++ methods/accounts/06_modify_an_account.js
@@ -0,0 +1,44 @@
+/**
+ * Modify an account
+ *
+ * Docs:
+ *   https://developers.coinbase.com/api#modify-an-account
+ * Lib:
+ *   Account.prototype.modify
+ *   https://github.com/coinbase/coinbase-node/blob/master/lib/model/Account.js
+ */
+
+var async   = require('async');
+var Account = require('coinbase').model.Account;
+var client  = require('../../client.js');
+
+
+var args = {
+ name: 'Satoshi Wallet'
+};
+
+async.waterfall([
+  function(callback) {
+    // Fetch an account
+    client.getAccounts(function(err, accounts) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        callback(null, accounts[0]);
+      }
+    });
+  }, function(sampleAccount, callback) {
+
+    var myAccount = new Account(client, sampleAccount);
+    // Alternatively, you can manually specify an account ID if needed
+    // var myAccount = new Account(client, {'id': 'A1234'});
+
+    myAccount.modify(args, function(err, modifiedAcct) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log(modifiedAcct);
+      }
+    });
+  }
+]);
