cf4792d2b7a09e28468bb63086d8def6356520e9
jborseth <jborseth@users.noreply.github.com>
2015-05-06 12:27:58 -0400

    Add 07_set_account_as_primary.js

---
+++ methods/accounts/07_set_account_as_primary.js
@@ -0,0 +1,40 @@
+/**
+ * Set account as primary
+ *
+ * Docs:
+ *   https://developers.coinbase.com/api#set-account-as-primary
+ * Lib:
+ *   Account.prototype.setPrimary
+ *   https://github.com/coinbase/coinbase-node/blob/master/lib/model/Account.js
+ */
+
+var async   = require('async');
+var Account = require('coinbase').model.Account;
+var client  = require('../../client.js');
+
+
+async.waterfall([
+  function(callback) {
+    // Fetch an account
+    client.getAccounts(function(err, accounts) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        callback(null, accounts[1]);
+      }
+    });
+  }, function(sampleAccount, callback) {
+
+    var myAccount = new Account(client, sampleAccount);
+    // To manually specify an account ID
+    // var myAccount = new Account(client, {'id': 'A1234'});
+
+    myAccount.setPrimary(function(err, result) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log(result);
+      }
+    });
+  }
+]);
