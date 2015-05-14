f4e01449fc1e6d19c9a1620c4403fc7af2f4f3ee
jborseth <jborseth@users.noreply.github.com>
2015-05-14 11:48:52 -0400

    clean up transfers/02_show_a_transfer.js

--- methods/transfers/02_show_a_transfer.js
+++ methods/transfers/02_show_a_transfer.js
@@ -1,52 +1,55 @@
 /**
- * Show a transfer - returns the transfer associated with a `transfer_id`
+ * Show a transfer
+ *
+ * Returns the transfer associated with a `transfer_id`
  *
  * Docs:
- *   https://developers.coinbase.com/api#list-buy-and-sell-history
+ *   https://developers.coinbase.com/api#show-a-transfer
  * Lib:
  *   Accounts.prototype.getTransfer
  *   https://github.com/coinbase/coinbase-node/blob/master/lib/model/Account.js
  */
- 
+
 var client = require('../../client.js');
 var Account = require('coinbase').model.Account;
 var async  = require('async');
 
+
 async.waterfall([
   function(callback){
-  // Fetch an account ID
-    client.getAccounts(function(err, accounts){
-      if(err){
-        console.log(err.message);
+    // Fetch an account ID
+    client.getAccounts(function(err, accounts) {
+      if (err) {
+        console.log(err);
       } else {
-        callback( null, accounts[0].id);
+        callback(null, accounts[0].id);
       }
     });
-  }, function( accountId, callback){
-      
-      // Optionally, you can manually specify an account ID here if needed
-      // var accountId = '';
-      
-      var myAccount = new Account( client, {"id" : accountId });
+  }, function(accountId, callback) {
 
-      myAccount.getTransfers( 1, 25, function(err, transfers){
-        if (err){
-          console.log(err.message);
-        } else {
-          callback(null, myAccount, transfers[0].id);
-        }
-      });
-    }, function( myAccount, transferId, callback){
-      
-      // Optionally, you can manually specify a transfer ID here if needed
-      // var transferId = '';
+    // Optionally, you can manually specify an account ID here if needed
+    // var accountId = '';
 
-      myAccount.getTransfer( transferId, function(err, transfers){
-        if (err){
-          console.log(err.message);
-        } else {
-          console.log(transfers);
-        }
-      });
-    },
+    var myAccount = new Account(client, {id: accountId});
+
+    myAccount.getTransfers(1, 25, function(err, transfers) {
+      if (err) {
+        console.log(err);
+      } else {
+        callback(null, myAccount, transfers[0].id);
+      }
+    });
+  }, function(myAccount, transferId, callback) {
+
+    // Optionally, you can manually specify a transfer ID here if needed
+    // var transferId = '';
+
+    myAccount.getTransfer(transferId, function(err, transfer) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(transfer);
+      }
+    });
+  },
 ]);
