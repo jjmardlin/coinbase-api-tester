5840947aea03521845267c4d23ce3ad7fde86c12
jborseth <jborseth@users.noreply.github.com>
2015-05-14 11:38:27 -0400

    Clean up transfers/01_list_buy_and_sell_history.js

--- methods/transfers/01_list_buy_and_sell_history.js
+++ methods/transfers/01_list_buy_and_sell_history.js
@@ -7,35 +7,34 @@
  *   Accounts.prototype.getTransfers
  *   https://github.com/coinbase/coinbase-node/blob/master/lib/model/Account.js
  */
- 
+
 var client = require('../../client.js');
 var Account = require('coinbase').model.Account;
 var async  = require('async');
 
 async.waterfall([
-  function(callback){
-  // Fetch an account ID
-    client.getAccounts(function(err, accounts){
-      if(err){
-        console.log(err.message);
+  function(callback) {
+    // Fetch an account ID
+    client.getAccounts(function(err, accounts) {
+      if (err) {
+        console.log(err);
       } else {
-        callback( null, accounts[0].id);
+        callback(null, accounts[0].id);
       }
     });
-  }, function( accountId, callback){
-      
-      // Optionally, you can manually specify an account ID here if needed
-      // var accountId = '';
-      
-      
-      var myAccount = new Account( client, {"id" : accountId });
+  }, function(accountId, callback) {
 
-      myAccount.getTransfers( 1, 25, function(err, transfers){
-        if (err){
-          console.log(err.message);
-        } else {
-          console.log(transfers);
-        }
-      });
-    }
+    // Optionally, you can manually specify an account ID here if needed
+    // var accountId = '';
+
+    var myAccount = new Account(client, {id: accountId});
+
+    myAccount.getTransfers(1, 25, function(err, transfers) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(transfers);
+      }
+    });
+  }
 ]);
